9ddc665af6c393e5ff60a04604d65520cd356a85
ysugimoto <sugimoto@wnotes.net>
2018-01-14 03:01:48 +0900

    WIP manipulation

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const parser = require('./lib/block/parser.js');
+const parser = require('./lib/parser/index.js');
+const Tree = require('./lib/tree.js');
 
-module.exports = parser;
+module.exports = mdString => {
+  const ast = parser(mdString);
+
+  return new Tree(ast);
+};
+
+module.exports.parse = parser;
